ba5499c36f90a75189a47e6aaf749e81efb4810c
Kenan Millet <KVMillet@Gmail.com>
2017-03-12 14:02:49 -0400

    Oops

--- Command/CommandEssentials.h
+++ Command/CommandEssentials.h
@@ -1,6 +1,8 @@
 #ifndef CommandEssentials_h
 #define CommandEssentials_h
 
+#include "Command.h"
+
 extern Command<unsigned char, unsigned char> cmd_pinMode;
 
 extern Command<unsigned char, unsigned char> cmd_digitalWrite;
